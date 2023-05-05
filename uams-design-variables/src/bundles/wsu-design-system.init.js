ae3aada8cf308d897028f7cbd266704b3ea92ce1
todd-uams <todd@uams.edu>
2023-05-05 11:25:01 -0500

    File name fixes

--- uams-design-variables/src/bundles/wsu-design-system.init.js
+++
@@ -1,8 +0,0 @@
-// Add has-js class to root html tag
-document.documentElement.classList.add('uams-has-js');
-
-import hasReducedMotion from '../../_assets/js/partials/hasReducedMotion';
-
-if ( ! hasReducedMotion() ) {
-    document.documentElement.classList.add('uams-reduce-motion');
-}
